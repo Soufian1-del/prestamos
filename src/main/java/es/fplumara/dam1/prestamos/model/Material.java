56271005460c59e29be39b0d080b86b98ad25d4c
sourrxz <sourrxz@gmail.com>
2026-02-16 10:26:19 +0100

    terminadas las clases abstractas,Enums, Interfaces y implementacions del model, Closes #Modelo(Poo)

--- src/main/java/es/fplumara/dam1/prestamos/model/Material.java
+++ src/main/java/es/fplumara/dam1/prestamos/model/Material.java
@@ -1,4 +1,41 @@
 package es.fplumara.dam1.prestamos.model;
 
-public class Material {
+import java.util.Set;
+
+public abstract class Material implements Identificable{
+    private String id;
+    private String nombre;
+    private EstadoMaterial estado;
+    private Set<String> etiquetas;
+    //Constructor
+
+    public Material(Set<String> etiquetas, EstadoMaterial estado, String nombre, String id) {
+        this.etiquetas = etiquetas;
+        this.estado = estado;
+        this.nombre = nombre;
+        this.id = id;
+    }
+    //Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public EstadoMaterial getEstado() {
+        return estado;
+    }
+
+    public Set<String> getEtiquetas() {
+        return etiquetas;
+    }
+
+    public void setEstado(EstadoMaterial estado) {
+        this.estado = estado;
+    }
+
+    public abstract String getTipo();
+
 }
--- src/main/java/es/fplumara/dam1/prestamos/model/prestamo.java
+++ src/main/java/es/fplumara/dam1/prestamos/model/prestamo.java
@@ -1,4 +1,15 @@
 package es.fplumara.dam1.prestamos.model;
 
-public class prestamo {
+import java.time.LocalDate;
+
+public class prestamo implements Identificable{
+    private String id;
+    private String idMaterial;
+    private String profesor;
+    private LocalDate fecha;
+
+    @Override
+    public String getID() {
+        return "";
+    }
 }
