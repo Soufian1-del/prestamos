1c057a6ec7ecb10231ef8a4aa0f6487da1e40692
sourrxz <sourrxz@gmail.com>
2026-02-16 10:53:25 +0100

    terminadas todas las implementaciones e intefaces de repository, Closes #Repositorios

---
+++ src/main/java/es/fplumara/dam1/prestamos/repository/MaterialRepositoryImpl.java
@@ -0,0 +1,10 @@
+package es.fplumara.dam1.prestamos.repository;
+
+import es.fplumara.dam1.prestamos.model.EstadoMaterial;
+import es.fplumara.dam1.prestamos.model.Material;
+
+import java.util.Set;
+
+public class MaterialRepositoryImpl extends BaseRepository<Material>{
+
+}
---
+++ src/main/java/es/fplumara/dam1/prestamos/repository/PrestamoRepositoryImpl.java
@@ -0,0 +1,6 @@
+package es.fplumara.dam1.prestamos.repository;
+
+import es.fplumara.dam1.prestamos.model.prestamo;
+
+public class PrestamoRepositoryImpl extends BaseRepository<prestamo>{
+}
